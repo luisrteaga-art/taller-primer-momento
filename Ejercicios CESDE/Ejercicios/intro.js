4bb5a38efc968d4220ee2e838b63b4af2d23025d
luisrteaga-art <luis.rteaga@gmail.com>
2026-08-08 01:53:44 -0500

    Add files via upload

---
+++ Ejercicios CESDE/Ejercicios/intro.js
@@ -0,0 +1,120 @@
+//  //Datos de entrada
+// let sueldo = prompt("Ingrese sueldo de empleado");
+// let salud;
+// let pension;
+// let salarioApagar;
+// //Proceso o calculos
+// salud = sueldo * 0.04;
+// pension = sueldo * 0.06;
+// salarioApagar = sueldo - (salud + pension)
+// //Datos de salida
+// alert(
+//     "Sueldo del empleado: " + sueldo + "\n" +
+//     "El descuento de Salud: "+ salud + "\n" +
+//     "El descuento de Salud: "+ pension + "\n" +
+//     "Salario a Pagar es: "+ salarioApagar + "\n"
+// );
+
+
+// //Ejercicio 2
+// // Variables
+// let base = prompt("Ingrese la base del triangulo");
+// let altura = prompt("Ingrese la altura del triangulo");
+// //Proceso o calculos
+// area = base * altura/2;
+// alert(
+//     "El area del triangulo: "+ area
+// );
+// console.log(
+//     "El area del triangulo: "+ area  
+// );
+
+// // Ejercicio 3
+// const numero1 = Number(prompt("Ingrese el número 1"));
+// const numero2  = Number(prompt("Ingrese número 2"));
+// //Proceso o calculos
+// suma = numero1 + numero2;
+// resta = numero1 - numero2;
+// multiplicacion = numero1*numero2;
+// division = numero1/numero2;
+// modulo = numero1%numero2;
+
+
+// alert(
+//     "La suma de los dos números es: "+ suma + "\n" +
+//     "La resta de los dos números es: "+ resta + "\n" +
+//     "La multiplicación de los dos números es: "+ multiplicacion + "\n" +
+//     "La división de los dos números es: "+ (division.toFixed(2)) + "\n" + 
+//     "El módulo de los dos números es: "+ modulo + "\n" 
+// );
+
+// //Ejercicio 4
+// let totalHoras = 210
+// let horasTrabajadas = prompt("Ingrese total de horas trabajadas");
+// //Proceso o calculos
+// let horasExtra = horasTrabajadas - totalHoras;
+// let horas = totalHoras;
+// if (horasTrabajadas > totalHoras){
+//     alert (
+//      "El total de horas trabajadas es: " + horasTrabajadas + "\n" +
+//     "El total de horas extras son: " + horasExtra + "\n"
+//     )
+    
+// }else {
+//     alert(
+    
+//     "El total de horas trabajadas es: " + horasTrabajadas + "\n" +
+//     "No aplica horas extra")
+// };
+
+// //Ejercicio 5
+// //Variables
+// let precio = Number(prompt ("digite el precio del producto"))
+// let iva = 0.19;
+// //Proceso o calculos
+// let valorIva = precio * iva;
+// let valorPagar = precio + valorIva;
+// alert(
+//     "Iva agregado: " + iva + "\n" +
+//     "El precio del producto es: " + precio + "\n" +
+//     "El valor a pagar: " + valorPagar
+// );
+
+// //Ejercicio 6
+// //Variables
+// let totalMujeres = Number(prompt("Digite la cantidad de mujeres"));
+// let totalHombres = Number(prompt("Digite la cantidad de hombres"));
+// //Proceso o calculos
+// let totalAlumnos = totalHombres + totalMujeres;
+// let porcentajeMujeres = totalAlumnos * totalMujeres / 100;
+// let porcentajeHombres = totalAlumnos * totalHombres / 100;
+
+// alert(
+//     "El total de alumnos son: " + totalAlumnos + "\n" +
+//     "El porcentaje de los hombres son: " + porcentajeHombres + "\n" +
+//     "El porcentaje de los mujeres son: " + porcentajeMujeres + "\n"
+// );
+
+// //Ejercicio 7
+// // Variables
+// let nota1 = Number(prompt("Digite su primera nota"))
+// let nota2 = Number(prompt("Digite su segunda nota"))
+// let nota3 = Number(prompt("Digite su tercera nota"))
+// //Proceso o calculos
+// let totalNotas = nota1 + nota2 + nota3;
+// let promedio = totalNotas / 3;
+
+// alert(
+//     "Su promedio es: " + promedio.toFixed(1)
+// );
+
+
+
+
+
+
+
+
+
+
+
